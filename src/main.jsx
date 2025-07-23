13b21d5594f4219defcf787dc3d9dce36feb4133
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-07-23 20:43:30 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'  // ✅ 바꿔줘야 함
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import { ProductProvider } from './contexts/ProductContext.jsx';
 
-console.log("✅ main.jsx 시작됨"); // 이거 추가해
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <HashRouter>
-      <App />
+      <ProductProvider>
+        <App />
+      </ProductProvider>
     </HashRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
