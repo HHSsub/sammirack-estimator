0d6062d3ad09cecf51fd5419ab6cc30ff87169b9
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-08-27 19:18:06 +0900

    fix fax홈피 분리

--- src/components/EstimateForm.jsx
+++ src/components/EstimateForm.jsx
@@ -247,6 +247,8 @@ const EstimateForm = () => {
             <tr>
               <td class="label">FAX</td>
               <td>(02)2611-4595</td>
+            </tr>
+            <tr>
               <td class="label">홈페이지</td>
               <td>http://www.ssmake.com</td>
             </tr>
@@ -422,6 +424,8 @@ const EstimateForm = () => {
           <tr>
             <td className="label">FAX</td>
             <td>(02)2611-4595</td>
+          </tr>
+          <tr>
             <td className="label">홈페이지</td>
             <td>http://www.ssmake.com</td>
           </tr>
