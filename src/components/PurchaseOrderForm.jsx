03dccafb0aeda66938eceffcaa7c0392dce3df24
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-08-27 18:54:19 +0900

    Update PurchaseOrderForm.jsx

--- src/components/PurchaseOrderForm.jsx
+++ src/components/PurchaseOrderForm.jsx
@@ -511,8 +511,6 @@ const PurchaseOrderForm = () => {
           <tr>
             <td className="label">FAX</td>
             <td>(02)2611-4595</td>
-          </tr>
-          <tr>
             <td className="label">홈페이지</td>
             <td>http://www.ssmake.com</td>
           </tr>
