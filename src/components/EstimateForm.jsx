1aae3c7e561e4f399c2a0c39f565907aaf4bc683
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-08-29 20:22:34 +0900

    Update EstimateForm.jsx

--- src/components/EstimateForm.jsx
+++ src/components/EstimateForm.jsx
@@ -254,7 +254,7 @@ const EstimateForm = () => {
           </tr>
           <tr>
             <td className="label">소재지</td>
-            <td>경기도 광명시 원노온사로 39, 제1동</td>
+            <td>경기도 광명시 원노온사로 39, 철제 스틸하우스 1</td>
             <td className="label">홈페이지</td>
             <td>http://www.ssmake.com</td>
           </tr>
