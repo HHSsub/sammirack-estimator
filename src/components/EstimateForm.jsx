0f02da884ac93041354cbaa0bf12de7bc2dee7ac
HHSsub <86719426+HHSsub@users.noreply.github.com>
2025-12-12 15:04:56 +0900

    Update EstimateForm.jsx

--- src/components/EstimateForm.jsx
+++ src/components/EstimateForm.jsx
@@ -464,7 +464,7 @@ const EstimateForm = () => {
                         updateFormData('documentNumber', e.target.value);
                       }}
                       placeholder=""
-                      style={{padding:'6px 8px', fontSize:'13px', width:'100%'}}
+                      style={{padding:'6px 8px', fontSize:'18px', fontWeight:'bold', color:'#ff6600', width:'100%'}}
                     />
                   </div>
                 </div>
